3c4829591ad39e9e54f563e9c8eb002daf5c27fd
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2023-11-04 20:23:50 -0500

    added lines for testing

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from followlist import FollowList    
+
 def main():
-    pass
+    # Initialize FollowList object
+    path = "list.txt"
+    following_list = FollowList()
+    following_list.load_copypaste(path)
+    following_list.save()
+    
+    # Initialize comparing object
+    comparePath = "testlist.txt"
+    compare_list = FollowList()
+    compare_list.load_copypaste(comparePath)
+    
+    # Initialize
+    following_list.compare(compare_list, printResults=True)
 
 if __name__ == '__main__':
     main()
